ec52b8cd23df3c9589c935e6a8b35a2beeb58065
Aduomas <adomasval04@gmail.com>
2021-08-24 21:02:15 +0300

    uploading current changes

--- src/reshop/main.py
+++ src/reshop/main.py
@@ -2,6 +2,11 @@ from requests_html import HTMLSession
 import csv
 import os
 import logging
+import configparser
+import io
+import sys
+
+# make a logger.
 
 
 def fetch(r):
@@ -57,29 +62,34 @@ def update(file_name, sheet):
                     print("added: \n", list_difference[0]) # what's new only, not what's removed tho.
                 else:
                     list_difference = [item for item in oldSheet if item not in sheet]
-                    print("removed: \n", list_difference[0])
+                    if list_difference:
+                        print("removed: \n", list_difference[0])
     else:
         exportData(sheet, file_name)
+        print(f"exported data into {file_name}")
 
 urls = [
     'https://reshop.lt/collections/klaviaturos',
     'https://reshop.lt/collections/pelytes',
     'https://reshop.lt/collections/ausines?limit=100'
 ]
+print(os.getcwd())
+config = configparser.ConfigParser()
+config.read('./config/config.ini')
 
-# should be placed in config
+
 file_names = [
-    'data_keyboards.csv',
-    'data_mouses.csv',
-    'data_headsets.csv'
+    config["file"]["file_keyboards"],
+    config["file"]["file_mouses"],
+    config["file"]["file_headsets"]
 ]
-
 
 session = HTMLSession()
 
 for count, url in enumerate(urls):
     r = session.get(url)
-    r.html.render(sleep=1)
+    render_sleep = config['main']['render_sleep']
+    r.html.render(sleep=2)
     sheet = fetch(r)
     print(f"checking for changes in {file_names[count]}")
     update(file_names[count], sheet)
