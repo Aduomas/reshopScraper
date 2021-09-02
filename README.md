8dc3c27c31e1f576f09adb376c14f08ee467d6c9
Aduomas <adomasval04@gmail.com>
2021-09-02 20:42:02 +0300

    reshopSraper 1.5
    Added command line -s SAVE to force save into files.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Practise Task P1
 
+In order to test scraper's ability to check for changes get 2021-08-25 scraper data from:
+https://drive.google.com/drive/folders/19_7O6smmC0zuRMJ9D4DAPcBcfmXKmz-D?usp=sharing and put it in root folder.
+
 Web scraper for reshop.lt
--- src/reshop/main.py
+++ src/reshop/main.py
@@ -1,47 +1,48 @@
 from requests_html import HTMLSession
-from multiprocessing.dummy import Pool
 import csv
 import os
 import configparser
 import io
 import sys
 import logging
+import argparse
 
 # make a logger.
 
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(levelname)s] - %(asctime)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    format="[%(levelname)s] - %(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler('./log/main.log', encoding='utf-8-sig'),
-        logging.StreamHandler()
-    ]
+        logging.FileHandler("./log/main.log", encoding="utf-8-sig"),
+        logging.StreamHandler(),
+    ],
 )
 logger = logging.getLogger(__name__)
-
-
 
 
 def fetch(r):
     items = r.html.xpath('//div[@id="gf-products"]/div')
 
-    sheet = [['Name', 'Price', 'Link']]
+    sheet = [["Name", "Price", "Link"]]
 
     for item in items:
-        raw_name = item.xpath('//a/text()')
-        name = ''
+        raw_name = item.xpath("//a/text()")
+        name = ""
         for s in raw_name:
-            if s == '\n':
+            if s == "\n":
                 continue
             name += s
 
-        link = 'https://reshop.lt' + item.xpath('//a/@href')[0]
+        link = "https://reshop.lt" + item.xpath("//a/@href")[0]
 
-
-        if (price := item.xpath('//span[@class="spf-product-card__saleprice money"]/text()')):
+        if price := item.xpath(
+            '//span[@class="spf-product-card__saleprice money"]/text()'
+        ):
             price = price[0]
-        elif (price := item.xpath('//span[@class="spf-product-card__price money"]/text()')):
+        elif price := item.xpath(
+            '//span[@class="spf-product-card__price money"]/text()'
+        ):
             price = price[0]
 
         sheet.append([name, price, link])
@@ -49,14 +50,14 @@ def fetch(r):
 
 
 def export_data(sheet, fileName):
-    with open(fileName, 'w', newline='', encoding='utf-8-sig') as file:
+    with open(fileName, "w", newline="", encoding="utf-8-sig") as file:
         writer = csv.writer(file)
         writer.writerows(sheet)
 
 
-def update(file_name, sheet):
+def update(file_name, sheet, to_save):
     if os.path.isfile(file_name):
-        with open(file_name, 'r', encoding='utf-8-sig') as file:
+        with open(file_name, "r", encoding="utf-8-sig") as file:
             reader = csv.reader(file)
             oldSheet = []
             for line in reader:
@@ -65,41 +66,53 @@ def update(file_name, sheet):
             if oldSheet != sheet:
                 list_difference = [item for item in sheet if item not in oldSheet]
                 if list_difference:
-                    logging.info('added: \n'+ str(list_difference[0]))
+                    logging.info("added: \n" + str(list_difference[0]))
                 list_difference = [item for item in oldSheet if item not in sheet]
                 if list_difference:
-                    logging.info('removed: \n' + str(list_difference[0]))                    
+                    logging.info("removed: \n" + str(list_difference[0]))
     else:
         export_data(sheet, file_name)
-        logging.info(f'exported data into {file_name}')
+        logging.info(f"exported data into {file_name}")
+    if to_save and oldSheet != sheet:
+        export_data(sheet, file_name)
+        logging.info(f"exported data into {file_name}")
 
 
 urls = [
-    'https://reshop.lt/collections/klaviaturos',
-    'https://reshop.lt/collections/pelytes',
-    'https://reshop.lt/collections/ausines?limit=100',
+    "https://reshop.lt/collections/klaviaturos",
+    "https://reshop.lt/collections/pelytes",
+    "https://reshop.lt/collections/ausines?limit=100",
 ]
 config = configparser.ConfigParser()
-config.read('./config/config.ini')
+config.read("./config/config.ini")
 
 
 file_names = [
-    config['file']['file_keyboards'],
-    config['file']['file_mouses'],
-    config['file']['file_headsets'],
+    config["file"]["file_keyboards"],
+    config["file"]["file_mouses"],
+    config["file"]["file_headsets"],
 ]
 
-logging.info('initializing the session')
+logging.info("initializing the session")
 session = HTMLSession()
 
 
-for count, url in enumerate(urls):
-    logging.info('')
+# command line arguments
+parser = argparse.ArgumentParser(description="Description")
+parser.add_argument("-s", "--Save", help="Save updates into the file")
+args = parser.parse_args()
+to_save = False
+if args.Save:
+    to_save = True
 
+
+for count, url in enumerate(urls):
+    logging.info("")
 
     r = session.get(url)
-    render_sleep = int(config['main']['render_sleep'])
+    render_sleep = int(config["main"]["render_sleep"])
     r.html.render(sleep=render_sleep)
     sheet = fetch(r)
-    logging.info(f'checking for changes in {file_names[count]}')
-    update(file_names[count], sheet)
+    logging.info(f"checking for changes in {file_names[count]}")
+
+    update(file_names[count], sheet, to_save)
